15f9dad8125b54d4ec7b9a590e8e0457673896cf
htn0312 <92318189+htn0312@users.noreply.github.com>
2021-10-18 12:48:38 +0300

    Create ufo_functions.hpp

---
+++ ufo_functions.hpp
@@ -0,0 +1,9 @@
+// functions file for ufo.cpp
+
+#include <vector>
+
+void greet();
+void display_misses(int misses);
+void display_status(std::vector<char> incorrect, std::string answer);
+void end_game(std::string answer, std::string codeword);
+// Declare functions
